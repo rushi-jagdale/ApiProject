92197ced311dd130a9b5e117d0ac36e5452b4015
rushi.jagdale <rjcom99@gmail.com>
2021-07-03 18:09:43 +0530

    first commit

--- Apiapp/urls.py
+++ Apiapp/urls.py
@@ -2,7 +2,7 @@
 from django.urls import  path
 from . import views
 from rest_framework.authtoken.views import obtain_auth_token
-from Apiapp.views import RegistrationAPIView,AdvisorViewSet
+from Apiapp.views import RegistrationAPIView,AdvisorViewSet,ViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -10,6 +10,8 @@ urlpatterns = [
     #path('api/v1/auth/auth-token', obtain_auth_token, name='obtain-auth-token')
     path('auth/register/',views.RegistrationAPIView.as_view(), name='register'),
     path('admin/advisor/',views.AdvisorViewSet.as_view(), name='advisor'),
+    path('admin/book/',views.ViewSet.as_view(), name='bookadvisor'),
+
     path('auth/login/', TokenObtainPairView.as_view(), name='login'),
     path('auth/refresh-token', TokenRefreshView.as_view(), name='refreshtoken'),
 
